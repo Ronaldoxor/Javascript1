b4a8eda90d9b2819a75af5f47593c32cf3fddf91
Ronaldo <rronaldo.monteiro.almeida@gmail.com>
2025-03-31 09:13:40 -0300

    Tipos de variáveis-typeoff

--- src/js/script.js
+++ src/js/script.js
@@ -12,6 +12,24 @@ console.log(sobrenome);
 //declarando uma variável indefinida
 let nome1;
 console.log(nome1);
+
 //declarando uma variável nula
 let nome2=null;
 console.log(nome2);
+
+//TIPOS DE VARIÁVEIS
+
+let exemplo1 = 10;
+console.log(typeof exemplo1);
+
+let exemplo2 ="Tecnologia";
+console.log(typeof exemplo2);
+
+let exemplo3 = true;
+console.log(typeof exemplo3);
+
+let exemplo4 ={};
+console.log(typeof exemplo4)
+
+let exemplo5 =["aluno1","aluno2"];
+console.log(typeof exemplo5);
