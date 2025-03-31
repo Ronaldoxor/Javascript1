164a5de508023ada4c210efdf43ac61982adf99a
Ronaldo <rronaldo.monteiro.almeida@gmail.com>
2025-03-31 09:07:44 -0300

    Declaração e variáveis

---
+++ src/js/script.js
@@ -0,0 +1,17 @@
+//DECLARAÇÕES E VARIAVEIS
+
+var nome ="fiap"
+console.log(nome);
+
+let idade =19;
+console.log(idade);
+
+const sobrenome="Fiapinho";
+console.log(sobrenome);
+
+//declarando uma variável indefinida
+let nome1;
+console.log(nome1);
+//declarando uma variável nula
+let nome2=null;
+console.log(nome2);
